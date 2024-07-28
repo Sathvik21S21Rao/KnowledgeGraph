3272b369d5873939eac28962641f603f22997b57
Sathvik <Sathvik.Rao@iiitb.ac.in>
2024-07-28 16:30:38 +0530

    altered prompts and added retry in case of failure

--- Graph_Generation/graph_extraction.py
+++ Graph_Generation/graph_extraction.py
@@ -1,67 +1,95 @@
 from langchain_community.llms.ollama import Ollama
 from langchain_core.prompts import PromptTemplate
-from langchain_text_splitters import CharacterTextSplitter
-# from prompts import *
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 import os
 import langchain_core.messages.ai
-from langchain_core.pydantic_v1 import BaseModel,Field
+from pydantic_core import from_json
+
+from pydantic import BaseModel,Field
 from typing import List, Tuple
 from Graph_Generation.prompts import ENTITY_TYPE_PROMPT,ENTITY_EXTRACTION_PROMPT,RELATION_PROMPT
-import tqdm
+from tqdm import tqdm
 class Entity_type(BaseModel):
-    entity_type :list =Field(description="The entity types found",)
+    
+    entity_type :list =Field(description="The entity types found",example=["type1","type2"])
 
 class Entity(BaseModel):
-    entity_name:List[str]=Field(description="The entities found")
-    entity_type:List[str]=Field(description="The entity types corresponding to the entities")
-    entity_description:List[str]=Field(description="The description of the corresponding entities")
+    entity_name:List[str]=Field(description="The entities found",example=["entity1","entity2"])
+    entity_type:List[str]=Field(description="The entity types corresponding to the entities",example=["type1","type2"])
+    entity_description:List[str]=Field(description="The description of the corresponding entities",example=["description1","description2"])
+
 class Relation(BaseModel):
-    relation:List[Tuple[str,str,str]]=Field(description="The relations found between the entities along with description")
+    relation:List[Tuple[str,str,str]]=Field(description="The relations found between the entities along with description",example=[("entity1","entity2","relation description"),("entity2","entity3","relation description")])
     
 class GraphExtractionChain:
     def __init__(self,llm):
-        self.llm=[llm.with_structured_output(Entity_type),llm.with_structured_output(Entity),llm.with_structured_output(Relation)]
+        self.llm=llm
         self.templates=[PromptTemplate(template=ENTITY_TYPE_PROMPT,input_variables=["task","input_text"]),PromptTemplate(template=ENTITY_EXTRACTION_PROMPT,input_variables=["entity_types","input_text","prev_entities"]),PromptTemplate(template=RELATION_PROMPT,input_variables=["entities","input_text"])]
     
-    def _extract_entity_types(self,task,input_text,prev_entities="None")->Entity_type:
-        chain=self.templates[0] | self.llm[0]
+    def _extract_entity_types(self,task,input_text,prev_entities="None",retries=3)->Entity_type:
+        chain=self.templates[0] | self.llm
         response=chain.invoke({"task":task,"input_text":input_text,"prev_entities":prev_entities})
+        if isinstance(response,langchain_core.messages.ai.AIMessage):
+            response=response.content
+        response=response[response.find("{"):response.rfind("}")]+"}"
+       
         try:
-            return response
-        except:
-            raise ValueError(f"Error in extracting entity types.The LLM is unable to return the output in the expected format.")
+            
+            print(from_json(response,allow_partial=True))
+            return Entity_type.model_validate(from_json(response,allow_partial=True))
+        except Exception as e:
+            if(retries>0):
+                return self._extract_entity_types(task,input_text,prev_entities,retries-1)
+            return Entity_type(entity_type=[])
     
-    def _extract_entities(self,entity_types,input_text,prev_entities="None")->Entity:
-        chain=self.templates[1] | self.llm[1]
+    def _extract_entities(self,entity_types,input_text,prev_entities="None",retries=3)->Entity:
+        chain=self.templates[1] | self.llm
         response=chain.invoke({"entity_types":entity_types,"input_text":input_text,"prev_entities":prev_entities})
+        if isinstance(response,langchain_core.messages.ai.AIMessage):
+            response=response.content
+        response=response[response.find("{"):response.rfind("}")+1]
+
         try:
-            return response
+            print(from_json(response,allow_partial=True))
+            return Entity.model_validate(from_json(response,allow_partial=True))
         except:
-            raise ValueError(f"Error in extracting entities.The LLM's response is {response}.The LLM is unable to return the output in the expected format.")
+            if(retries>0):
+                return self._extract_entities(entity_types,input_text,prev_entities,retries-1)
+            return Entity(entity_name=[],entity_type=[],entity_description=[])
     
-    def _extract_relations(self,entities,input_text)->Relation:
+    def _extract_relations(self,entities,input_text,retries=3)->Relation:
        
         entities=",".join(entities)
         
-        chain=self.templates[2] | self.llm[2]
+        chain=self.templates[2] | self.llm
         response=chain.invoke({"entities":entities,"input_text":input_text})
+        if isinstance(response,langchain_core.messages.ai.AIMessage):
+            response=response.content
+        response=response[response.find("{"):response.rfind("}")+1]
+        
         try:
-            return response
-        except:
-            raise ValueError(f"Error in extracting relations. The LLM's response is {response}. The LLM is unable to return the output in the expected format.")
+            print(eval(response))
+            return Relation.model_validate(eval(response))
+        except Exception as e:
+            if(retries>0):
+                return self._extract_relations(entities,input_text,retries-1)
+            return Relation(relation=[])
     
     
     def execute(self,input_text,prev_entities):
-        task="Identify classes for the objects in the given text. Classes could be person, location, organization, concept, etc."
+        task="Identify classes for the objects in the given text. Classes could be person, location, organization, concept,item,living thing,technology,thought,time,activity etc.Feel free to generate new classes which are relavant to the text."
         entity_types=self._extract_entity_types(task,input_text)
         entities=self._extract_entities(entity_types,input_text,prev_entities=prev_entities)
+        if(entities.entity_name==[]):
+            return entities,Relation(relation=[])
         relations=self._extract_relations(entities.entity_name,input_text)
         return entities,relations
     
     
 class DataLoader:
-    def __init__(self,path,chunk_size=512):
-        self.text_splitter=CharacterTextSplitter(chunk_size=chunk_size)
+    def __init__(self,path,chunk_size,chunk_overlap):
+        self.text_splitter=RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
         self.path=path
     def load(self):
         if os.path.isdir(self.path):
@@ -90,21 +118,25 @@ class PrepareDataForNX:
     def load_data_from_llm(self,data,chain:GraphExtractionChain):
         final_entities={}
         final_relations={}
-        for i in tqdm(range(len(data)),desc="Chunk"):
-            text=data[i]
-            entities,relations=chain.execute(text,",".join(list(final_entities.keys())))
-            for entity,description in zip(entities.entity_name,entities.entity_description):
-                
-                if final_entities.get(entity):
-                    final_entities[entity.lower()]+=description
-                else:
-                    final_entities[entity.lower()]=description
+       
+        with tqdm(total=len(data), desc="Processing Chunks", unit="chunk") as pbar:
+            for i in range(len(data)):
+                text=data[i]
+                entities,relations=chain.execute(text,",".join(list(final_entities.keys())))
+                for entity,description in zip(entities.entity_name,entities.entity_description):
+                    
+                    if final_entities.get(entity):
+                        final_entities[entity.lower()]+=description
+                    else:
+                        final_entities[entity.lower()]=description
 
-            for relation in relations.relation:
-                if final_relations.get((relation[0].lower(),relation[1].lower())):
-                    final_relations[(relation[0].lower(),relation[1].lower())]+="."+relation[2]
-                else:
-                    final_relations[(relation[0].lower(),relation[1].lower())]=relation[2]
+                for relation in relations.relation:
+                    if final_relations.get((relation[0].lower(),relation[1].lower())):
+                        final_relations[(relation[0].lower(),relation[1].lower())]+="."+relation[2]
+                    else:
+                        final_relations[(relation[0].lower(),relation[1].lower())]=relation[2]
+                pbar.update(1)
+                pbar.set_postfix({"Current Chunk": i+1, "Remaining": len(data) - (i+1)})
         return final_entities,final_relations
         
     def execute(self,data,chain):
--- Graph_Generation/prompts.py
+++ Graph_Generation/prompts.py
@@ -7,29 +7,38 @@ Avoid general entity types such as "other" or "unknown".
 This is VERY IMPORTANT: Do not generate redundant or overlapping entity types. For example, if the text contains "company" and "organization" entity types, you should return only one of them.
 Don't worry about quantity, always choose quality over quantity. And make sure EVERYTHING in your answer is relevant to the context of entity extraction.
 And remember, it is ENTITY TYPES what we need.
-Return the entity types in as a list of comma sepparated of strings.
+Return the entity types as shown in the example
+Do not return any other information, not even the reasoning, just the output
 =====================================================================
 EXAMPLE SECTION: The following section includes example output. These examples **must be excluded from your answer**.
 
 EXAMPLE 1
 Task: Determine the connections and organizational hierarchy within the specified community.
-Text: Example_Org_A is a company in Sweden. Example_Org_A's director is Example_Individual_B.
+Text: Example_Org_A is a company in Sweden founded in 1998. Example_Org_A's director is Example_Individual_B.
 RESPONSE:
-organization, person
+
+{{"entity_type": ["organization", "person", "time"]}}
+
+
 END OF EXAMPLE 1
 
 EXAMPLE 2
 Task: Identify the key concepts, principles, and arguments shared among different philosophical schools of thought, and trace the historical or ideological influences they have on each other.
 Text: Rationalism, epitomized by thinkers such as René Descartes, holds that reason is the primary source of knowledge. Key concepts within this school include the emphasis on the deductive method of reasoning.
 RESPONSE:
-concept, person, school of thought
-END OF EXAMPLE 2
 
+
+{{"entity_type":["concept", "person", "school of thought"]}}
+
+END OF EXAMPLE 2
 EXAMPLE 3
 Task: Identify the full range of basic forces, factors, and trends that would indirectly shape an issue.
 Text: Industry leaders such as Panasonic are vying for supremacy in the battery production sector. They are investing heavily in research and development and are exploring new technologies to gain a competitive edge.
 RESPONSE:
-organization, technology, sectors, investment strategies
+
+{{"entity_type":
+  ["organization", "technology", "sectors", "investment strategies"]}}
+
 END OF EXAMPLE 3
 
 ======================================================================
@@ -53,14 +62,16 @@ Given a text document that is potentially relevant to this activity and a list o
 - entity_description: Comprehensive description of the entity's attributes and activities
 2. Do not have pronouns as entities (for example, he/she, him/her, they/them, etc.); use the previously recognized entities as a reference. Do not create variation names for the same entity; use the previous entities as a reference. Previous entities: {prev_entities}
 3. If an entity is mentioned multiple times in the text, provide a single description that captures all relevant information.
+4. Do not return any other information, not even the reasoning, just the output
+5. Return the output as shown in the example
 ######################
 -Examples-
 ######################
 Example 1:
 
-Entity_types: [person, technology, mission, organization, location]
+Entity_types: [person, technology, mission, organization, location,time]
 Text:
-while Alex clenched his jaw, the buzz of frustration dull against the backdrop of Taylor's authoritarian certainty. It was this competitive undercurrent that kept him alert, the sense that his and Jordan's shared commitment to discovery was an unspoken rebellion against Cruz's narrowing vision of control and order.
+while Alex clenched his jaw at noon, the buzz of frustration dull against the backdrop of Taylor's authoritarian certainty. It was this competitive undercurrent that kept him alert, the sense that his and Jordan's shared commitment to discovery was an unspoken rebellion against Cruz's narrowing vision of control and order.
 Then Taylor did something unexpected. They paused beside Jordan and, for a moment, observed the device with something akin to reverence. “If this tech can be understood..." Taylor said, their voice quieter, "It could change the game for us. For all of us.”
 The underlying dismissal earlier seemed to falter, replaced by a glimpse of reluctant respect for the gravity of what lay in their hands. Jordan looked up, and for a fleeting heartbeat, their eyes locked with Taylor's, a wordless clash of wills softening into an uneasy truce.
 It was a small transformation, barely perceptible, but one that Alex noted with an inward nod. They had all been brought here by different paths
@@ -68,16 +79,18 @@ It was a small transformation, barely perceptible, but one that Alex noted with 
 #############
 Output:
 
-
-  "entity_name": ["Alex", "Taylor", "Jordan", "Cruz"],
-  "entity_type": ["person", "person", "person", "person"],
+{{
+  "entity_name": ["Alex", "Taylor", "Jordan", "Cruz","Noon","Device"],
+  "entity_type": ["person", "person", "person", "person","time","technology"],
   "entity_description": [
     "Alex is a member of a team committed to discovery, who noted a transformation in Taylor's behavior.",
     "Taylor showed an unexpected reverence for a device, indicating its potential significance.",
     "Jordan shared a commitment to discovery with Alex and had a moment of mutual understanding with Taylor.",
-    "Cruz has a vision of control and order that contrasts with Alex and Jordan's commitment to discovery."
+    "Cruz has a vision of control and order that contrasts with Alex and Jordan's commitment to discovery.",
+    "Noon is the time of day when Alex's jaw clenched.",
+    "Device is a piece of technology that Taylor showed reverence for."
   ]
-
+}}
 
 
 ###############
@@ -106,15 +119,16 @@ From the entities identified in step 1, identify all pairs of (source_entity, ta
 3. Do not generate variations of entity names, use the one's which are provided.
 
 4. Do not miss out any possible relationship between entities.
-
+5. Do not return any other information, not even the reasoning, just the output
+6. Return the output as shown in the example.
  ######################
 -Examples-
 ######################
 Example 1:
 
-Entities: Alex,Taylor,Jordan,Cruz,The Device
+Entities: Alex,Taylor,Jordan,Cruz,Noon,The Device
 Text:
-while Alex clenched his jaw, the buzz of frustration dull against the backdrop of Taylor's authoritarian certainty. It was this competitive undercurrent that kept him alert, the sense that his and Jordan's shared commitment to discovery was an unspoken rebellion against Cruz's narrowing vision of control and order.
+while Alex clenched his jaw at noon, the buzz of frustration dull against the backdrop of Taylor's authoritarian certainty. It was this competitive undercurrent that kept him alert, the sense that his and Jordan's shared commitment to discovery was an unspoken rebellion against Cruz's narrowing vision of control and order.
 
 Then Taylor did something unexpected. They paused beside Jordan and, for a moment, observed the device with something akin to reverence. “If this tech can be understood..." Taylor said, their voice quieter, "It could change the game for us. For all of us.”
 
@@ -124,7 +138,7 @@ It was a small transformation, barely perceptible, but one that Alex noted with 
 ################
 Output:
 
-("Alex","Taylor","Alex is affected by Taylor's authoritarian certainty and observes changes in Taylor's attitude towards the device."),("Alex","Jordan","Alex and Jordan share a commitment to discovery, which contrasts with Cruz's vision."),("Alex","Jordan","Alex and Jordan share a commitment to discovery, which contrasts with Cruz's vision."),("Taylor","Jordan","Taylor and Jordan interact directly regarding the device, leading to a moment of mutual respect and an uneasy truce."),("Taylor","The Device","Taylor shows reverence towards the device, indicating its importance and potential impact."),("Jordan","Cruz","Jordan's commitment to discovery is in rebellion against Cruz's vision of control and order."),("Taylor","The Device","Taylor shows reverence towards the device, indicating its importance and potential impact.")
+{{"relation":[("Alex","Taylor","Alex is affected by Taylor's authoritarian certainty and observes changes in Taylor's attitude towards the device."),("Alex","Jordan","Alex and Jordan share a commitment to discovery, which contrasts with Cruz's vision."),("Alex","Jordan","Alex and Jordan share a commitment to discovery, which contrasts with Cruz's vision."),("Alex","Noon","Alex clenched his jaw at Noon"),("Taylor","Jordan","Taylor and Jordan interact directly regarding the device, leading to a moment of mutual respect and an uneasy truce."),("Taylor","The Device","Taylor shows reverence towards the device, indicating its importance and potential impact."),("Jordan","Cruz","Jordan's commitment to discovery is in rebellion against Cruz's vision of control and order."),("Taylor","The Device","Taylor shows reverence towards the device, indicating its importance and potential impact.")]}}
 
 
 
