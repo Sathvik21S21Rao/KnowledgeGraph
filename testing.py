3f26a20c60aafc610ec3745e3b4a23d8b9f9fa3d
sathvik <sathvik.rao@iiitb.ac.in>
2024-08-17 22:51:46 +0530

    adding testing code

---
+++ testing.py
@@ -0,0 +1,191 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import unittest
+import pickle
+import os
+import time
+import networkx as nx
+from checkDataUpdates.checkFileUpdates import SyncData, create_temp_folder
+from Graph_Generation.graph_extraction import GraphExtractionChain, DataLoader, PrepareDataForNX, UpdateGraph
+from Community_Generation.communitySummary import UpdateCommunities
+from Graph_Retrieval.context_based_node_retrieval import ContextBasedNodeRetrieval
+from Graph_Retrieval.query import Query
+
+
+# Function definitions
+def load_graph(graph_path):
+    with open(graph_path, "rb") as f:
+        return pickle.load(f)
+
+def save_graph(graph, graph_path):
+    with open(graph_path, "wb") as f:
+        pickle.dump(graph, f)
+
+def initialize_llm(config):
+    if config.get("server").lower() == "openai" and config.get("api_key"):
+        from langchain_openai.chat_models import ChatOpenAI
+        return ChatOpenAI(api_key=config["api_key"], model=config["model"], temperature=config["temperature"])
+    elif config.get("server").lower() == "ollama":
+        from langchain_experimental.llms.ollama_functions import OllamaFunctions
+        return OllamaFunctions(model=config["model"], temperature=config["temperature"])
+    elif config.get("server").lower() == "groq":
+        from langchain_groq import ChatGroq
+        return ChatGroq(temperature=config["temperature"], model=config["model"], api_key=config.get("api_key"))
+    else:
+        raise ValueError("Invalid server configuration")
+    
+def initialize_embedding_model(config):
+    embedding_server = config.get("embedding_server").lower()
+
+    if embedding_server == "ollama":
+        from langchain_community import embeddings
+        return embeddings.OllamaEmbeddings(model=config.get("embedding_model", ""))
+    elif embedding_server == "huggingface":
+        from langchain_community import embeddings
+        return embeddings.HuggingFaceEmbeddings(model=config.get("embedding_model", ""))
+    elif embedding_server == "openai":
+        from langchain_community import embeddings
+        return embeddings.OpenAIEmbeddings(model=config.get("embedding_model", ""), api_key=config.get("api_key"))
+    elif embedding_server == "local":
+        return None
+
+
+api_key = os.getenv("API_KEY")
+
+config = {
+    "data_path": "data_test",
+    "chunk_size": 512,
+    "chunk_overlap": 128,
+    "api_key": api_key,
+    "server": "Groq",
+    "model": "llama-3.1-8b-instant",
+    "temperature": 0.5,
+    "use_sentence_embeddings": False,
+    "node2vec_model_path": "./model_test/node2vec.model",
+    "sentence_model_path": "./model_test/sentence.model",
+    "node2vec_embeddings_path": "./embeddings_test/node2vec_embeddings.npy",
+    "graph_file_path": "./graph_test/graph.pkl",
+    "collection_name": "node_data_test",
+    "node_data_dir": "./node_data_test",
+    "community_data_dir": "./community_data_test",
+    "sentence_embeddings_path": "embeddings_test/sentence_embeddings.npy",
+    "node_names_path": "./embeddings_test/node_names.npy",
+    "sentence_model_name": "all-MiniLM-L6-v2",
+    "faiss_model_path": "./model_test/faiss.index"
+}
+
+graph_file_path = config.get("graph_file_path")
+node2vec_model_path = config.get("node2vec_model_path")
+node_data_dir = config.get("node_data_dir")
+community_data_dir = config.get("community_data_dir")
+chunk_size = config.get("chunk_size")
+chunk_overlap = config.get("chunk_overlap")
+sentence_model_path = config.get("sentence_model_path")
+node2vec_embeddings_path = config.get("node2vec_embeddings_path")
+sentence_embeddings_path = config.get("sentence_embeddings_path")
+node_names_path = config.get("node_names_path")
+faiss_model_path = config.get("faiss_model_path")
+sentence_model_name = config.get("sentence_model_name")
+use_sentence_embeddings = config.get("use_sentence_embeddings")
+llm = initialize_llm(config)
+
+# Unit test class for graph creation
+class TestGraphCreation(unittest.TestCase):
+    def test_graph_creation(self):
+        """Test graph creation from data"""
+        chain = GraphExtractionChain(llm=llm)
+        data = DataLoader(path=config["data_path"], chunk_size=chunk_size, chunk_overlap=chunk_overlap).load()
+        NxData = PrepareDataForNX().execute(data, chain)
+        create_temp_folder(config["data_path"],community_data_dir)
+        graph = nx.Graph()
+        graph.add_nodes_from(NxData[0])
+        graph.add_edges_from(NxData[1])
+        save_graph(graph, graph_file_path)
+        obj = ContextBasedNodeRetrieval(llm, graph, node2vec_model_path, node_data_dir, community_data_dir, True)
+        obj.setup()
+        self.assertTrue(os.path.exists(graph_file_path))
+        loaded_graph = load_graph(graph_file_path)
+        self.assertIsInstance(loaded_graph, nx.Graph)
+        self.assertEqual(len(graph.nodes), len(loaded_graph.nodes))
+        self.assertEqual(len(graph.edges), len(loaded_graph.edges))
+
+class TestGraphUpdate(unittest.TestCase):
+    def setUp(self):
+        """Setup the initial graph and other resources"""
+        # Create an initial graph
+        self.graph=load_graph(graph_file_path)
+        time.sleep(10)
+
+    def test_graph_updation(self):
+        """Test graph updation after detecting changes in the data"""
+        # Assume some updates have been made in the data directory
+        # Simulate sync and graph update
+        update_data="""As the friends settled back into their daily lives, the memories of their adventure lingered. They often gathered to reminisce, each retelling of their journey bringing new details and laughter. However, the island's allure was not easily forgotten. Raj, ever the adventurer, proposed a return trip, this time with better preparation and equipment. The idea was met with a mix of excitement and apprehension.
+
+Months passed, and the group found themselves drawn back to the island, not just for the thrill but to uncover its secrets. They spent their time researching, gathering advanced gear, and honing their survival skills. Harish designed a more reliable GPS system, while Vikram studied the island's flora and fauna in greater detail. Neha and Priya took first aid courses, ensuring they were ready for any emergencies.
+
+When they finally set foot on the island again, it felt both familiar and new. This time, they were determined to explore the parts they had missed. They ventured deeper into the jungle, discovering hidden waterfalls, ancient ruins, and mysterious caves. Each discovery brought a sense of wonder and accomplishment.
+
+One evening, as they sat around the campfire, Asha shared a theory she had been developing. She believed the island held more than just natural beauty; it might be the site of a lost civilization. The ruins they had found hinted at advanced architecture and tools. Intrigued, the group decided to focus their efforts on uncovering more about this potential civilization.
+
+Days turned into weeks as they meticulously documented their findings. They unearthed artifacts, deciphered ancient symbols, and pieced together the island's history. Their discoveries attracted the attention of historians and archaeologists, turning their adventure into a significant academic pursuit.
+
+The island, once a place of peril and mystery, became a beacon of knowledge and exploration. The friends' bond grew stronger as they worked together, each contributing their unique skills. Their story evolved from a tale of survival to one of discovery and legacy.
+
+Years later, their findings were published, and the island was recognized as a site of historical importance. The friends, now celebrated explorers and scholars, often returned to the island, not just as adventurers but as guardians of its secrets. Their journey had come full circle, transforming them from curious graduates to pioneers of knowledge.
+
+And so, the legend of "The Lost Expedition" continued to inspire, reminding everyone that with courage, curiosity, and camaraderie, even the most daunting challenges could lead to extraordinary discoveries.
+
+So, the story is the same, but the formatting is different. The formatting in the .temp/data file is more readable, with line breaks and indentation. The formatting in the .temp/data/data file is a single block of text, making it harder to read. This shows the importance of formatting in making text more accessible and easier to read."""
+
+        with open(config["data_path"] + "/data", "a") as f:
+            f.write(update_data)
+        sync = SyncData(folder=config["data_path"], temp_folder="./.temp")
+        updates = sync.compareFolders()
+        
+        if updates:
+            updates = "\n".join(updates)
+            updates = DataLoader(path=None, chunk_overlap=chunk_overlap, 
+            chunk_size=chunk_size).load_text(updates)
+            sync.syncTempFolder()
+            chain = GraphExtractionChain(llm=llm)
+            updated_graph = load_graph(graph_file_path)
+            updated_nodes, added_nodes, added_edges = UpdateGraph(
+                graph=updated_graph, graph_path=graph_file_path
+            ).execute(updates, chain)
+            
+            updated_nodes = [node[0] for node in updated_nodes + added_nodes]
+            
+            # Update communities based on the new graph structure
+            UpdateCommunities(
+                graph=updated_graph, llm=llm, community_dir=community_data_dir,
+                create=False, updated_nodes=updated_nodes
+            ).update_communities()
+            
+            save_graph(updated_graph, graph_file_path)
+        
+        # Load the updated graph and perform assertions
+        loaded_graph = load_graph(graph_file_path)
+        self.assertIsInstance(loaded_graph, nx.Graph)
+        self.assertGreaterEqual(len(loaded_graph.nodes), len(self.graph.nodes))
+        self.assertGreaterEqual(len(loaded_graph.edges), len(self.graph.edges))
+    
+class TestQuery(unittest.TestCase):
+    def setUp(self):
+        """Setup the initial graph and other resources"""
+        # Create an initial graph
+        self.graph = load_graph(graph_file_path)
+        self.obj = ContextBasedNodeRetrieval(llm, self.graph, node2vec_model_path,
+        node_data_dir, community_data_dir, False)
+        self.obj.setup()
+        
+    def test_query(self):
+        """Test query execution"""
+        query = "What is the story of the lost expedition?"
+        context = self.obj.get_context(query=query)
+        response = Query(context, query, llm, chat_history=[]).execute_query()
+        self.assertIsInstance(response, str)
+        self.assertTrue(response)
+
+
